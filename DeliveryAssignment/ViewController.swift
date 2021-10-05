e2cca1c11fd89384e6c06b0b8b791aed993ef407
Yl <Yl@naver.com>
2021-10-05 14:16:35 +0900

    10.05 Nav, Tab Controller practice

--- DeliveryAssignment/ViewController.swift
+++ DeliveryAssignment/ViewController.swift
@@ -7,13 +7,191 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstTabViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("first :  \(#function)")
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("first :  \(#function)")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("first :  \(#function)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("first :  \(#function)")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("first :  \(#function)")
+    }
+    
+    
 
 
 }
 
+class SecondTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("second :  \(#function)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("second :  \(#function)")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("second :  \(#function)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("second :  \(#function)")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("second :  \(#function)")
+    }
+    
+}
+
+class ThirdTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("third :  \(#function)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("third :  \(#function)")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("third :  \(#function)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("third :  \(#function)")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("third :  \(#function)")
+    }
+    
+
+}
+
+class ThirdTableViewController : UITableViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("third table :  \(#function)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("third table :  \(#function)")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("third table :  \(#function)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("third table :  \(#function)")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("third table :  \(#function)")
+    }
+}
+
+class ThirdTmpViewCont : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("third tmp :  \(#function)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("third tmp :  \(#function)")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("third tmp :  \(#function)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("third tmp :  \(#function)")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("third tmp :  \(#function)")
+    }
+}
+
+
+class ThirdModalViewCont : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("third modal :  \(#function)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("third modal :  \(#function)")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("third modal :  \(#function)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("third modal :  \(#function)")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("third modal :  \(#function)")
+    }
+}
+
+
+
+class LastTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("last :  \(#function)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("last :  \(#function)")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("last :  \(#function)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("last :  \(#function)")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("last :  \(#function)")
+    }
+}
